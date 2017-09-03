d06fd7ab860c72046cd8e54ed353ab5825d833a2
satoru <satorulogic@gmail.com>
2017-09-03 12:25:30 +0800

    Use setup.py to upload package

--- README.rst
+++ README.rst
@@ -1,8 +1,18 @@
 plain_obj
 ##########
 
+.. image:: https://travis-ci.org/suzaku/plain_obj.svg?branch=master
+    :target: https://travis-ci.org/suzaku/plain_obj
+.. image:: https://img.shields.io/pypi/v/plain_obj.svg
+    :target: https://pypi.python.org/pypi/plain_obj
+
+A faster alternative to namedtuple.
+
 .. image:: https://app.codesponsor.io/embed/MY7qFCdB7bDgiBqdjtV9ASYi/suzaku/plain_obj.svg
     :width: 888px
     :height: 68px
     :alt: Sponsor
     :target: https://app.codesponsor.io/link/MY7qFCdB7bDgiBqdjtV9ASYi/suzaku/plain_obj
+
+
+
---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+
+setup(
+    name='plain_obj',
+    version='0.1.0',
+    description='A faster alternative to namedtuple.',
+    long_description=long_description,
+    url='https://github.com/suzaku/plain_obj',
+    license='MIT',
+    author='Satoru Logic',
+    author_email='satorulogic@gmail.com',
+    py_modules=['plain_obj'],
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='tuple',
+)
