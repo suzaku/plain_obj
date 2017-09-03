ac168fccca184490ab9d0796efa729cf764ef89e
satoru <satorulogic@gmail.com>
2017-09-03 11:44:56 +0800

    Add implementation with tests

---
+++ plain_obj.py
@@ -0,0 +1,55 @@
+from keyword import iskeyword
+from collections import OrderedDict
+
+
+def new_type(type_name, field_names):
+    if isinstance(field_names, str):
+        # names separated by whitespace and/or commas
+        field_names = field_names.replace(',', ' ').split()
+    check_name(type_name)
+    seen_fields = set()
+    for name in field_names:
+        check_name(name)
+        if name in seen_fields:
+            raise ValueError('Encountered duplicate field name: %r' % name)
+        seen_fields.add(name)
+    return type(
+        type_name,
+        (PlainBase,),
+        {
+            '__slots__': field_names,
+            '__init__': make_constructor(field_names)
+        }
+    )
+
+
+class PlainBase(object):
+
+    def __iter__(self):
+        for name in self.__slots__:
+            yield getattr(self, name)
+
+    def __repr__(self):
+        values = tuple(self)
+        return self.__class__.__name__ + repr(values)
+
+    def to_dict(self):
+        return OrderedDict(zip(self.__slots__, self))
+
+
+def make_constructor(fields):
+    assignments = '\n'.join(['    self.%s = %s' % (f, f) for f in fields])
+    parameter_lists = ', '.join(fields)
+    source = 'def __init__(self, %s):\n%s' % (parameter_lists, assignments)
+    namespace = {}
+    exec(source, {}, namespace)
+    return namespace['__init__']
+
+
+def check_name(name):
+    if not all(c.isalnum() or c == '_' for c in name):
+        raise ValueError('Type names and field names can only contain alphanumeric characters and underscores: %r' % name)
+    if iskeyword(name):
+        raise ValueError('Type names and field names cannot be a keyword: %r' % name)
+    if name[0].isdigit():
+        raise ValueError('Type names and field names cannot start with a number: %r' % name)
---
+++ test.py
@@ -0,0 +1,56 @@
+import pytest
+
+import plain_obj
+
+
+class TestCreateNewType:
+
+    def test_can_create_with_a_list_of_fields(self):
+        Point = plain_obj.new_type('Point', ['x', 'y', 'z'])
+        assert isinstance(Point, type)
+        assert len(Point.__slots__) == 3
+
+    def test_can_create_with_comma_separated_string(self):
+        Point = plain_obj.new_type('Point', 'x,y,z')
+        assert isinstance(Point, type)
+        assert len(Point.__slots__) == 3
+
+    def test_can_create_with_space_separated_string(self):
+        Point = plain_obj.new_type('Point', 'x y  z')
+        assert isinstance(Point, type)
+        assert len(Point.__slots__) == 3
+
+
+class TestValidation:
+
+    def test_should_report_when_name_is_invalid(self):
+        invalid_names = ['123', 'f(&*']
+        for n in invalid_names:
+            with pytest.raises(ValueError):
+                plain_obj.new_type('Point', ['123'])
+
+    def test_should_report_when_name_is_keyword(self):
+        with pytest.raises(ValueError):
+            plain_obj.new_type('Point', ['is'])
+
+
+def test_create_new_objects():
+    Config = plain_obj.new_type('Config', 'skips_dist,run_tests')
+    config = Config(True, False)
+    assert config.skips_dist
+    assert not config.run_tests
+
+
+def test_repr():
+    Point = plain_obj.new_type('Point', 'x y z')
+    point = Point(3, 1, 4)
+    assert repr(point) == 'Point(3, 1, 4)'
+
+
+def test_to_dict():
+    Config = plain_obj.new_type('Config', 'skips_dist,run_tests')
+    config = Config(True, False)
+    assert config.to_dict() == {
+        'skips_dist': True,
+        'run_tests': False,
+    }
